1f41133ca7994915fe7f10a0e5cb4de4234aa4cb
Jason S. Jones <jsjones96@gmail.com>
2019-09-28 18:17:11 -0700

    chore: move config files to root of project

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    printWidth: 100,
+    tabWidth: 4,
+    overrides: [
+        {
+            files: '*.md',
+            options: {
+                parser: 'markdown',
+                printWidth: 120,
+                'prose-wrap': 'preserve'
+            }
+        }
+    ]
+};
